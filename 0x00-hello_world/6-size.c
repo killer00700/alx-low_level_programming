a3a7e412fca4fec8ec25221b74e66c200051973a
abdel <test@gmail.com>
2023-08-10 17:16:09 -0700

    dvsdf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("size of a int : %lu byt(s)\n", sizeof(int));
 	printf("size of a long int : %lu byt(s)\n", sizeof(long int));
 	printf("size of a long long int : %lu byt(s)\n", sizeof(long long int));
-	printf("size of a float : %lu byt(s) \n", sizeof(float));
+	printf("size of a float : %lu byt(s)\n", sizeof(float));
 	return (0);
 }
