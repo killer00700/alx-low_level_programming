3d8515434ea76a473333870c61293dec474c8c9b
abdel <test@gmail.com>
2023-08-10 17:45:34 -0700

    sdvas

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("size of a char : %lu byte(s)\n",(unsigned long)sizeof(char));
+	printf("size of a char : %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("size of a int : %lu byt(s)\n", (unsigned long)sizeof(int));
 	printf("size of a long int : %lu byt(s)\n", (unsigned long)sizeof(long int));
 	printf("size of a long long int : %lu byt(s)\n", (unsigned long)sizeof(long long int));
