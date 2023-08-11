02bdb01679d1fcb7dccaf267fa212f04583419a4
abdel <test@gmail.com>
2023-08-10 17:41:26 -0700

    dacADF

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char : %u byte(s)\n", sizeof(char));
-	printf("size of a int : %u byt(s)\n", sizeof(int));
-	printf("size of a long int : %u byt(s)\n", sizeof(long int));
-	printf("size of a long long int : %u byt(s)\n", sizeof(long long int));
-	printf("size of a float : %u byt(s)\n", sizeof(float));
+	printf("size of a char : %lu byte(s)\n", sizeof(char));
+	printf("size of a int : %lu byt(s)\n", sizeof(int));
+	printf("size of a long int : %lu byt(s)\n", sizeof(long int));
+	printf("size of a long long int : %lu byt(s)\n", sizeof(long long int));
+	printf("size of a float : %lu byt(s)\n", sizeof(float));
 	return (0);
 }
